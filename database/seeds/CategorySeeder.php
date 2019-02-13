6b1d2417eea5cc2620647eb2659e58e136105217
Jai-Code <jaicode.kh@gmail.com>
2019-02-13 22:53:00 +0700

    Post Method Added

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create([
+            'name' => 'Entertainment'
+        ]);
+
+        Category::create([
+            'name' => 'Social'
+        ]);
+
+        Category::create([
+            'name' => 'Technology'
+        ]);
+    }
+}
